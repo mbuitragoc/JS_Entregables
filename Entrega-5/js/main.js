3d0f7a8bee02443fb66c589eb7f98b31215424dc
mbuitragoc <mbuitragocastillo@gmail.com>
2022-06-27 22:08:17 -0500

    Added Ingresar y Retirar de la Guarderia

--- Entrega-5/js/main.js
+++ Entrega-5/js/main.js
@@ -9,7 +9,7 @@ class Mascota {
 }
 const Zaphi = new Mascota("Zaphi", 1, "F", "Perro", 1);
 const DonGato = new Mascota("DonGato", 3, "M", "Gato", 2);
-const enGuarderia = [Zaphi, DonGato, Zaphi, Zaphi, Zaphi];
+const enGuarderia = [Zaphi, DonGato];
 
 function mostrarGuarderia() {
   const guarderia = document.getElementById("guarderia");
@@ -24,7 +24,6 @@ function mostrarGuarderia() {
     card.classList.add("col-3");
     card.classList.add("p-2");
     if (mascota.especie == "Perro") {
-      console.log("perro");
       especie = "perro";
     } else if (mascota.especie == "Gato") {
       especie = "gato";
@@ -33,10 +32,36 @@ function mostrarGuarderia() {
     }
     card.innerHTML = `<img src="./icons/${especie}.jpg">
                       <h5 class="card-title pt-1"> ${mascota.id}. ${mascota.nombre}</h5>
-                      <p> Edad: ${mascota.edad}, Genero: ${mascota.genero}`;
+                      <p> Edad: ${mascota.edad}, Genero: ${mascota.genero}, ID: ${mascota.id}`;
     row.append(card);
   }
   guarderia.append(row);
 }
+
+document.getElementById("agregar").addEventListener("click", function () {
+  const nombre = document.getElementById("nombre").value;
+  const edad = document.getElementById("edad").value;
+  const genero = document.getElementById("genero").value;
+  const especie = document.getElementById("especie").value;
+  const id = enGuarderia.length + 1;
+  const mascota = new Mascota(nombre, edad, genero, especie, id);
+  enGuarderia.push(mascota);
+  document.getElementById(
+    "mensaje"
+  ).innerHTML = `<p>${nombre} ingreso en la Guarderia</p>`;
+  mostrarGuarderia();
+});
+
+document.getElementById("eliminar").addEventListener("click", function () {
+  const nombre = document.getElementById("nombre").value;
+  indexRetiro = enGuarderia.indexOf(
+    enGuarderia.find((e) => e.nombre == nombre)
+  );
+  enGuarderia.splice(indexRetiro, 1);
+  document.getElementById(
+    "mensaje"
+  ).innerHTML = `<p>${nombre} fue retirada de la Guarderia</p>`;
+  mostrarGuarderia();
+});
 
 mostrarGuarderia();
