3a2af20490296ad3f15aff2675d18f4ec8f0f45a
mbuitragoc <mbuitragocastillo@gmail.com>
2022-07-11 22:25:06 -0500

    Added better notifications and date management

---
+++ Entrega_8_Librerias/js/main.js
@@ -0,0 +1,124 @@
+const DateTime = luxon.DateTime;
+
+class Mascota {
+  constructor(nombre, edad, genero, especie, id) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.genero = genero;
+    this.especie = especie;
+    this.id = id;
+  }
+}
+
+let listaGuarderia = JSON.parse(localStorage.getItem("datosGuarderia"));
+listaGuarderia.forEach((element) => {
+  element.horaIngreso = DateTime.now();
+});
+
+function mostrarGuarderia() {
+  const guarderia = document.getElementById("guarderia");
+  guarderia.innerHTML = "";
+
+  let especie;
+  const row = document.createElement("div");
+  row.classList.add("row");
+  row.classList.add("gap-2");
+
+  for (const mascota of listaGuarderia) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("col-3");
+    card.classList.add("p-2");
+    mascota.especie == "Perro"
+      ? (especie = "perro")
+      : mascota.especie == "Gato"
+      ? (especie = "gato")
+      : (especie = "other");
+    card.innerHTML = `<img src="./icons/${especie}.jpg">
+                      <h5 class="card-title pt-1"> ${mascota.id}. ${mascota.nombre}</h5>
+                      <p> Edad: ${mascota.edad}, Genero: ${mascota.genero}, ID: ${mascota.id}`;
+    row.append(card);
+  }
+  guarderia.append(row);
+}
+
+document.getElementById("agregar").addEventListener("click", function () {
+  const nombre = document.getElementById("nombre").value;
+  const edad = document.getElementById("edad").value;
+  const genero = document.getElementById("genero").value;
+  const especie = document.getElementById("especie").value;
+  const id = listaGuarderia.length + 1;
+
+  if (nombre != "") {
+    const mascota = new Mascota(nombre, edad, genero, especie, id);
+    mascota.horaIngreso = DateTime.now();
+    listaGuarderia.push(mascota);
+    document.getElementById(
+      "mensaje"
+    ).innerHTML = `<p>${nombre} ingreso en la Guarderia</p>`;
+
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Ingreso en Guarderia Exitoso",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "No se pudo ingresar en Guarderia",
+      text: "Verifica los datos de la mascota",
+    });
+  }
+
+  mostrarGuarderia();
+  localStorage.setItem("datosGuarderia", JSON.stringify(listaGuarderia));
+});
+
+document.getElementById("eliminar").addEventListener("click", function () {
+  const nombre = document.getElementById("nombre").value;
+  indexRetiro = listaGuarderia.indexOf(
+    listaGuarderia.find((e) => e.nombre == nombre)
+  );
+
+  if (indexRetiro != -1) {
+    let horaRetiro = DateTime.now();
+    let estadia = horaRetiro.diff(
+      listaGuarderia[indexRetiro].horaIngreso
+    ).minutes;
+
+    console.log(estadia.minutes);
+
+    listaGuarderia.splice(indexRetiro, 1);
+    document.getElementById(
+      "mensaje"
+    ).innerHTML = `<p>${nombre} fue retirada de la Guarderia</p>
+                  <p> ${nombre} se quedo durante ${estadia} minutos`;
+
+    Swal.fire({
+      position: "top-end",
+      icon: "info",
+      title: "Retiro de la Guarderia Exitoso",
+      text: "Gracias por utilizar nuestros servicios",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "No se encuentra en Guarderia",
+      text: "Verifica el nombre de la mascota",
+    });
+  }
+
+  mostrarGuarderia();
+  localStorage.setItem("datosGuarderia", JSON.stringify(listaGuarderia));
+});
+
+localStorage.getItem("datosGuarderia")
+  ? mostrarGuarderia()
+  : (localStorage.setItem("datosGuarderia", JSON.stringify(datosGuarderia)),
+    mostrarGuarderia());
